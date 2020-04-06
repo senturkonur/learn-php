64e87961b5ff342de5dd436c305dce254ee45f6e
Onur Şentürk <onur.senturk@ideasoft.com.tr>
2020-04-06 14:36:25 +0300

    Associative Arrays done chp 4

--- Associative Arrays.php
+++ Associative Arrays.php
@@ -1,3 +1,4 @@
+<!--Challenge #1 of 3 -->
 <!--Our favorite pet - Waggy Pig - is a cute white silly dog.
 
 Your Mission
@@ -17,4 +18,28 @@ $waggyPig = [
 <h2><?php echo $waggyPig['name']; ?></h2>
 <div class="age"><?php echo $waggyPig['age']; ?></div>
 <div class="weight"><?php echo $waggyPig['weight']; ?></div>
+<p><?php echo $waggyPig['bio']; ?></p>
+
+<!--Challenge #1 of 3 -->
+<!--We went to ancestry.com and have discovered Waggy Pig's breed: bichon.
+
+Your Mission
+Add a new breed key set to bichon on a new line after the $waggyPig array has already been created.
+
+Print this new info below! -->
+$waggyPig = [
+'name' => 'Waggy Pig',
+'weight' => 10,
+'age' => 7,
+'bio' => 'Sleepy white fluffy dog',
+];
+$waggyPig['breed'] = 'bichon';
+?>
+
+<h2><?php echo $waggyPig['name']; ?></h2>
+<div class="age"><?php echo $waggyPig['age']; ?></div>
+<div class="weight"><?php echo $waggyPig['weight']; ?></div>
+<div class="breed">
+    <?php echo $waggyPig['breed']; ?>
+</div>
 <p><?php echo $waggyPig['bio']; ?></p>
